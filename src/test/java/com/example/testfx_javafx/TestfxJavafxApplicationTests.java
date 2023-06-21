09c1a9c68aadccd090c506ac4c8063d4316f2e6c
Rams200599 <136055767+Rams200599@users.noreply.github.com>
2023-06-21 01:50:37 -0400

    Update TestfxJavafxApplicationTests.java

--- src/test/java/com/example/testfx_javafx/TestfxJavafxApplicationTests.java
+++ src/test/java/com/example/testfx_javafx/TestfxJavafxApplicationTests.java
@@ -41,15 +41,15 @@ class TestfxJavafxApplicationTests extends ApplicationTest{
 	private static Scene scene;
 	private SecondaryController secondaryController;
 
-	// @BeforeClass
-	// public static void setUpHeadlessMode(){
-	// 		// System.setProperty("testfx.robot", "awt");
-	// 		System.setProperty("testfx.headless", "true");
-	// 		// System.setProperty("prism.order", "sw");
-	// 		// System.setProperty("prism.text", "t2k");
-	// 		// System.setProperty("java.awt.headless", "true");
-	// 	        // System.setProperty("monocle.platform", "Headless");
-	// }
+	@BeforeClass
+	public static void setUpHeadlessMode(){
+			System.setProperty("testfx.robot", "awt");
+			System.setProperty("testfx.headless", "true");
+			// System.setProperty("prism.order", "sw");
+			// System.setProperty("prism.text", "t2k");
+			System.setProperty("java.awt.headless", "true");
+		        System.setProperty("monocle.platform", "Headless");
+	}
 
 	
 
