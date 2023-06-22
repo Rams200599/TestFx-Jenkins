44c0685dec7a14649a4c91ba379c3ed945486a30
Rams200599 <136055767+Rams200599@users.noreply.github.com>
2023-06-22 06:54:14 -0400

    Update TestfxJavafxApplicationTests.java

--- src/test/java/com/example/testfx_javafx/TestfxJavafxApplicationTests.java
+++ src/test/java/com/example/testfx_javafx/TestfxJavafxApplicationTests.java
@@ -213,7 +213,7 @@ public void testButtonc1(){
 	clickOn("#buttonE");
 	sleep(500);
 
-	FxRobot robot = new FxRobot();
+	// FxRobot robot = new FxRobot();
 
 	//Test cse for Slider
 	Slider slider = robot.lookup("#slider").query();
