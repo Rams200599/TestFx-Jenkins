3332c606933c0a4d6e166e296afb478e21e7f22c
Rams200599 <136055767+Rams200599@users.noreply.github.com>
2023-06-22 06:26:15 -0400

    Update TestfxJavafxApplicationTests.java

--- src/test/java/com/example/testfx_javafx/TestfxJavafxApplicationTests.java
+++ src/test/java/com/example/testfx_javafx/TestfxJavafxApplicationTests.java
@@ -214,28 +214,28 @@ public void testButtonc1(){
 @Test
 public void testButtonD1(){
 	FxRobot robot = new FxRobot();
-	clickOn("#buttonD");
-	sleep(500);
+	// clickOn("#buttonD");
+	// sleep(500);
 
-	//Test case for ComboBox
-	// clickOn("#comboBOx");
-	clickOn(1015, 395);
-	sleep(500);
+	// //Test case for ComboBox
+	// // clickOn("#comboBOx");
+	// clickOn(1015, 395);
+	// sleep(500);
 
-	ComboBox b2 = robot.lookup("#comboBOx").query();
-	assertNotNull(b2);
-	assertEquals(5, b2.getItems().size());
+	// ComboBox b2 = robot.lookup("#comboBOx").query();
+	// assertNotNull(b2);
+	// assertEquals(5, b2.getItems().size());
 
 	//select an item in the ComboBox
-	clickOn("Aspirants");
-	interact(() -> b2.getSelectionModel().select("Aspirants"));
+	// clickOn("Aspirants");
+	// interact(() -> b2.getSelectionModel().select("Aspirants"));
 
-	//verify the selected item
-	assertEquals("Aspirants", b2.getSelectionModel().getSelectedItem());
-	sleep(500);
+	// //verify the selected item
+	// assertEquals("Aspirants", b2.getSelectionModel().getSelectedItem());
+	// sleep(500);
 
-	clickOn("#choiceBox");
-	sleep(500);
+	// clickOn("#choiceBox");
+	// sleep(500);
 
 	//Test case for choice box
 	ChoiceBox b1 = robot.lookup("#choiceBox").query();
