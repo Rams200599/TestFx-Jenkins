3e931324ea573534a5d827eccab6c7b3a7575af4
Rams200599 <136055767+Rams200599@users.noreply.github.com>
2023-06-22 06:49:55 -0400

    Update SecondaryController.java

--- src/main/java/com/example/testfx_javafx/SecondaryController.java
+++ src/main/java/com/example/testfx_javafx/SecondaryController.java
@@ -460,7 +460,7 @@ public class SecondaryController {
         label3.setLayoutY(230);
 
         Slider slider = new Slider();
-        slider.setId("Slider");
+        slider.setId("slider");
         slider.setLayoutX(180);
         slider.setLayoutY(90);
         slider.setMin(0);
